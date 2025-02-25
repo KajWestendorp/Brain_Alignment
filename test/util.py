be8d7536b4536e086feb90c26d3484447e4b520f
KajWestendorp <kaj.westendorp@student.uva.nl>
2025-02-25 14:23:23 +0100

    Updated the THINGS and tvsd dataloaders

--- test/util.py
+++ test/util.py
@@ -4,29 +4,26 @@ import os
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
+import h5py
+import numpy as np
+import torch 
 
 ############################# Low-level Functions: Dataset
-def get_things_dataloader(image_paths):
+
+def get_things_dataloader(image_paths, device="cuda"):
     """Dataloader for THINGS dataset"""
 
-    # Define the transform
+    # Define the transform (matching AlexNet's ImageNet normalization)
     transform = transforms.Compose([
-
-        # Resize to 224x224
-        transforms.Resize((224, 224)),  
-
-        # Convert to tensor for PyTorch
-        transforms.ToTensor(),  
-
-        # Normalize the images
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # Apparently this is the mean and std for ImageNet but gotta double check
+        transforms.Resize((224, 224)),  # Resize to 224x224
+        transforms.ToTensor(),  # Convert to tensor for PyTorch
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize for ImageNet (doulbe check this)
     ])
 
-    # Load the images and apply the transform
     # Collect the image paths and labels
     dataset = []
     labels = []
-    class_names = sorted(os.listdir(image_paths))  # Sort folders to have consistent labels
+    class_names = sorted(os.listdir(image_paths))  
 
     # Traverse the directories to load images and assign labels based on folder names
     for label, class_name in enumerate(class_names):
@@ -34,21 +31,73 @@ def get_things_dataloader(image_paths):
         
         if os.path.isdir(class_dir):
             for img_name in os.listdir(class_dir):
-                if img_name.endswith('.jpg'):
+                if img_name.endswith('.jpg'): 
                     img_path = os.path.join(class_dir, img_name)
                     image = Image.open(img_path).convert("RGB")
                     image = transform(image)
                     dataset.append(image)
                     labels.append(label)
 
-    # Return the dataloader
-    return DataLoader(dataset, batch_size=128, num_workers=4)
- 
-def get_NSD_dataloader(image_paths):
-    pass
+    # Send dataset and labels to the CUDA device
+    dataset = [image.to(device) for image in dataset]  
+    labels = torch.tensor(labels).to(device)  
 
-def get_tvsd(subject, normalized=True):
-    pass
+    # Return the dataloader
+    return DataLoader(list(zip(dataset, labels)), batch_size=128, num_workers=4)
+
+ 
+def get_NSD_dataloader(image_paths, device="cuda"):
+    """Dataloader for NSD dataset (perhaps could be the same as the THINGS dataset, depending on t he structure)"""
+    
+    # Define the transform (matching AlexNet's ImageNet normalization)
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # Resize to 224x224
+        transforms.ToTensor(),  # Convert to tensor for PyTorch
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize for ImageNet (doulbe check this)
+    ])
+
+    # Collect the image paths and labels
+    dataset = []
+    labels = []
+    class_names = sorted(os.listdir(image_paths))  
+
+    # Traverse the directories to load images and assign labels based on folder names
+    for label, class_name in enumerate(class_names):
+        class_dir = os.path.join(image_paths, class_name)
+        
+        if os.path.isdir(class_dir):
+            for img_name in os.listdir(class_dir):
+                if img_name.endswith('.jpg'): 
+                    img_path = os.path.join(class_dir, img_name)
+                    image = Image.open(img_path).convert("RGB")
+                    image = transform(image)
+                    dataset.append(image)
+                    labels.append(label)
+
+    # Send dataset and labels to the CUDA device
+    dataset = [image.to(device) for image in dataset]  
+    labels = torch.tensor(labels).to(device)  
+
+    # Return the dataloader
+    return DataLoader(list(zip(dataset, labels)), batch_size=128, num_workers=4)
+
+def get_tvsd(subject_file_path, normalized=True, device="cuda"):
+    """Dataloader for neurodata from TVSD dataset"""
+
+    # Load the data from the h5 file
+    with h5py.File(subject_file_path, "r") as file:
+        data_dict = {key: torch.tensor(np.array(file[key]), dtype=torch.float32, device=device) for key in file.keys()}
+
+    train_mua = data_dict.get("train_MUA")
+    
+    #Index the data into V1, V4, IT based on Papale paper
+    V1 = train_mua[:, :512]
+    V4 = train_mua[:, 512:768]
+    IT = train_mua[:, 768:1024]
+    
+    return V1, V4, IT  # Each is a tensor now
+
+
 
 def get_eeg(subject):
     pass
@@ -63,15 +112,24 @@ def get_fmri(subject):
 
 def get_neurodata(dataset_name, subjects, ephys_normalized=True):
 
-    get_tvsd(subjects, normalized=ephys_normalized)
+    if dataset_name == "tvsd":
+        get_tvsd(subjects, normalized=ephys_normalized)
+    elif dataset_name == "eeg":
+        get_eeg(subjects)
+    elif dataset_name == "fmri":
+        get_fmri(subjects)
+    
     pass
 
 
 def get_dataloader(dataset_name):
 
     # Check which dataset and respond accordingly
-    # Train dataloader and test dataloader, hardcode 
-    pass
+    # Train dataloader and test dataloader, hardcode for now, how would i do this?
+    if dataset_name == "THINGS":
+        get_things_dataloader()
+    elif dataset_name == "NSD":
+        get_NSD_dataloader()
 
 def get_model(model_name):
     pass
