c4afb32e0b838e0b3a2cc4ccfa394e01796f114b
KajWestendorp <kaj.westendorp@student.uva.nl>
2025-02-25 13:44:26 +0100

    THINGS dataloader function

--- test/util.py
+++ test/util.py
@@ -1,7 +1,49 @@
+################################## Imports
+
+import os
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+
 ############################# Low-level Functions: Dataset
 def get_things_dataloader(image_paths):
-    pass
+    """Dataloader for THINGS dataset"""
 
+    # Define the transform
+    transform = transforms.Compose([
+
+        # Resize to 224x224
+        transforms.Resize((224, 224)),  
+
+        # Convert to tensor for PyTorch
+        transforms.ToTensor(),  
+
+        # Normalize the images
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) # Apparently this is the mean and std for ImageNet but gotta double check
+    ])
+
+    # Load the images and apply the transform
+    # Collect the image paths and labels
+    dataset = []
+    labels = []
+    class_names = sorted(os.listdir(image_paths))  # Sort folders to have consistent labels
+
+    # Traverse the directories to load images and assign labels based on folder names
+    for label, class_name in enumerate(class_names):
+        class_dir = os.path.join(image_paths, class_name)
+        
+        if os.path.isdir(class_dir):
+            for img_name in os.listdir(class_dir):
+                if img_name.endswith('.jpg'):
+                    img_path = os.path.join(class_dir, img_name)
+                    image = Image.open(img_path).convert("RGB")
+                    image = transform(image)
+                    dataset.append(image)
+                    labels.append(label)
+
+    # Return the dataloader
+    return DataLoader(dataset, batch_size=128, num_workers=4)
+ 
 def get_NSD_dataloader(image_paths):
     pass
 
