e42c8870eab0187163bb080c5f68b3a7db1d47e8
onjurgensen <ole.jurgensen@student.uva.nl>
2025-03-04 11:26:30 +0100

    testing

--- test/oletests.py
+++ test/oletests.py
@@ -48,7 +48,18 @@ class THINGS(Dataset):
         if self.transform:
             img = self.transform(img)
         return img, 0., idx 
+
+def get_things_dataloader(transform, THINGS_PATH,train_imgs_paths, test_imgs_paths, batch_size=128, num_workers=4):
+    """Function to get the dataloader for the THINGS dataset"""
     
+    train_dataset = THINGS(root=THINGS_PATH, transform=transform, paths=train_imgs_paths)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
+
+    test_dataset = THINGS(root=THINGS_PATH, transform=transform, paths=test_imgs_paths)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
+
+    return train_dataloader, test_dataloader
+
 
 def get_tvsd(subject_file_path, normalized=True, device="cuda"):
     """Dataloader for neurodata from TVSD dataset, this function will return the data for a single subject separated into V1, V4, IT"""
@@ -75,3 +86,75 @@ def get_eeg(subject):
 def get_fmri(subject):
     pass
 
+
+
+############################# High-level Functions
+
+
+def get_model(model_name):
+    if model_name == 'alexnet':
+        model = torch.hub.load('pytorch/vision:v0.6.0', 'alexnet', pretrained=True)
+        #Add more later
+    return model
+
+
+def get_neurodata(dataset_name, subjects, ephys_normalized=True):
+    if dataset_name == "tvsd":
+        return get_tvsd(subjects, normalized=ephys_normalized)
+    elif dataset_name == "eeg":
+        return get_eeg(subjects)
+    elif dataset_name == "fmri":
+        return get_fmri(subjects)
+
+
+
+def get_dataloader(dataset_name, batch_size=128, num_workers=4):
+    """Function to get the dataloader for the specified dataset
+    
+    Args:
+        dataset_name (str): The name of the dataset to get the dataloader for
+        batch_size (int): The batch size for the dataloader
+        num_workers (int): The number of workers for the dataloader
+        
+    Returns:
+        train_dataloader, test_dataloader: The dataloaders for the training and testing sets
+    """
+    from sklearn.model_selection import train_test_split
+
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    # Check which dataset and respond accordingly
+    if dataset_name == 'THINGS':
+        #  Here you want to somehow define your split for training and testing (and I guess also for using the EEG THINGS dataset or the ephys THINGS dataset
+        # I did so using the train_tset_split function from sklearn Idk if that's the best way to do it
+        THINGS_PATH = os.path.expanduser("~/Documents/BrainAlign_Data/things_images/")  # Correct image directory
+        # MAT_FILE_PATH = os.path.expanduser("~/Documents/BrainAlign_Data/things_imgsF.mat")  # Separate .mat file
+        # Example usage
+        file_path = os.path.expanduser("~/Documents/BrainAlign_Data/things_imgsF.mat")
+        train_imgs_paths = get_image_paths(file_path, 'train_imgs')
+
+        # add the test_imgs to the all_imgs_paths
+        test_imgs_paths = get_image_paths(file_path, 'test_imgs')
+        # all_imgs_paths = train_imgs_paths + test_imgs_paths
+        img_directory = os.path.expanduser("~/Documents/BrainAlign_Data/object_images")
+
+
+        for i in range(len(train_imgs_paths)):
+            train_imgs_paths[i] = os.path.join(img_directory, os.path.normpath(train_imgs_paths[i].replace('\\', '/')))
+
+        # The below part should then go into the get_things_dataloader function which you then only call here
+        train_dataloader, test_dataloader = get_things_dataloader(transform,THINGS_PATH, train_imgs_paths, test_imgs_paths, batch_size=batch_size, num_workers=num_workers)
+
+        return train_dataloader, test_dataloader
+        
+    elif dataset_name == 'NSD':
+        ... # do NSD dataloader preparation
+
+    return train_dataloader, test_dataloader
+    
+
+################################## Feature Extraction functions
